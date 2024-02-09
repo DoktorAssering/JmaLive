68e226c47843674dd694c3dcfbb19b8bd63510f7
Андрей Кочегаров <andyafpooo@mail.ru>
2024-02-09 16:50:24 +0300

    Version 0.0.0.3

--- server/server/server.go
+++ server/server/server.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Тест прошёл успешно!"))
+	})
+
+	log.Fatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
+
+	log.Println("Сервер успешно запустился")
+}
