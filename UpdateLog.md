b9afc559c55669806c1daa9b58d4d194ece53d6b
Андрей Кочегаров <andyafpooo@mail.ru>
2024-02-09 19:16:36 +0300

    Version 0.0.0.4

---
+++ UpdateLog.md
@@ -0,0 +1,87 @@
+# Update Log 
+
+~~~~~~~~~~~~~~~~~~Version 0.0.0.1~~~~~~~~~~~~~~~~~~
+
+
+DATE: 07.02.2024
+
+~~~Backend~~~
+
+Creating Path for Frontend & Backend.
+
+~~~Backend~~~
+
+~~~Frontend~~~
+
+Created Architecture Project,
+Installed React.
+
+~~~Frontend~~~
+
+
+~~~~~~~~~~~~~~~~~~Version 0.0.0.2~~~~~~~~~~~~~~~~~~
+
+
+DATE: 07.02.2024
+
+~~~Backend~~~
+
+Creating Start file for Backend, - go.mod, server.go, handler.go, db.go, core.go
+
+~~~Backend~~~
+
+~~~Frontend~~~
+
+Created Home page, Registration page, Authorization page,
+Created Global Search.
+
+~~~Frontend~~~
+
+
+~~~~~~~~~~~~~~~~~~Version 0.0.0.3~~~~~~~~~~~~~~~~~~
+
+
+DATE: 09.02.2024
+
+~~~Backend~~~
+
+Created OpenSSL (cert.pem, key.pem) for HTTPS,
+Created log Path, and file server.log,
+Created Auto-logging ERROR from server,
+CReated Auto creation path for logs and auto creation himself server.log.
+
+~~~Backend~~~
+
+~~~Frontend~~~
+
+Created Page for media player,
+Configurated page for media player,
+Created media player for obs-studio.
+
+~~~Frontend~~~
+
+
+~~~~~~~~~~~~~~~~~~Version 0.0.0.4~~~~~~~~~~~~~~~~~~
+
+DATE: 09.02.2024
+
+~~~ForGit~~~
+
+Created UpdateLog.md,
+Updated UpdateLog.md.
+
+~~~ForGit~~~
+
+~~~Backend~~~
+
+Fixed "Received signal: interrupt", An error prevented all goroutines from being completely terminated,
+New logging lines,
+New func which waits for the server to stop working and performs the necessary functions before shutting down the server.
+
+~~~Backend~~~
+
+~~~Frontend~~~
+
+Testing OBS Studio translation on the web, EXODUS: DON'T WORK, fix in next update.
+
+~~~Frontend~~~
--- server/server/server.go
+++ server/server/server.go
@@ -1,16 +1,78 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
+)
+
+const (
+	port = ":443"
+	ip   = "127.0.0.1"
 )
 
 func main() {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logFilePath := filepath.Join(currentDir, "JmaLive", "server", "logs", "server.log")
+
+	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("os.OpenFile() filed with '%s\n", err)
+	}
+	defer logFile.Close()
+
+	log := log.New(logFile, "Server: ", log.LstdFlags|log.Lshortfile)
+	log.SetOutput(logFile)
+	log.Println("Log entry")
+
+	// Handle Funcs
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Тест прошёл успешно!"))
+		w.Write([]byte("Test successful!"))
 	})
 
-	log.Fatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
+	// Handle Funcs
 
-	log.Println("Сервер успешно запустился")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigChan
+		log.Printf("Received signal: %v", sig)
+		cancel()
+		time.Sleep(2 * time.Second)
+		os.Exit(0)
+	}()
+
+	fmt.Println("Starting the HTTP server...")
+	log.Printf("HTTPS server is starting with address: %s%s", ip, port)
+	server := &http.Server{Addr: port}
+	go func() {
+		if err := server.ListenAndServeTLS("JmaLive/server/server/cert.pem", "JmaLive/server/server/key.pem"); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+			fmt.Printf("Error starting server: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	log.Println("Shutting down the server...")
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatalf("Error shutting down server: %v", err)
+	}
+
+	// Some more end function
 }
